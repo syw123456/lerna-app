cbda0ed8b038259dd7ff105be23303ddc8909da2
arian <arian.shi@bybit.com>
2021-04-02 17:37:02 +0800

    feat: change

--- packages/feu-tools/lib/feu-tools.js
+++ packages/feu-tools/lib/feu-tools.js
@@ -4,6 +4,11 @@ function add(a, b) {
     console.log("tools库，调用函数add,入参：%d, %d",a ,b)
     return a + b;
 }
+function test(a, b) {
+    console.log("tools库，调用函数test,入参：%d, %d",a ,b)
+    return a.toString() + b.toString();
+}
+
 function min(a, b) {
     console.log("tools库，调用函数min,入参：%d, %d",a ,b)
     return Math.min(a, b);
